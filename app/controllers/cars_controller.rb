be7bd8af358c364efc89a8de71a0f8348069089d
Herbert M. <hmorales5@tutanota.com>
2024-01-01 07:14:17 -0600

    added ability to show and create cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,28 @@ class CarsController < ApplicationController
   def index
     @cars = Car.all
   end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+
+    if @car.save
+      redirect_to @car
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:make, :model, :year)
+  end
 end
